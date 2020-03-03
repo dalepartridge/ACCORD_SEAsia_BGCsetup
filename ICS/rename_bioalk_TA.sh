48edfaf078d8c26de4e73442d50c22803d33f65c
Dale Partridge <dapa@pml.ac.uk>
2020-03-03 15:43:24 +0000

    Restructure

--- ICS/rename_bioalk_TA.sh
+++
@@ -1,8 +0,0 @@
-module unload cray-netcdf-hdf5parallel cray-hdf5-parallel
-module load cray-netcdf cray-hdf5
-module load nco/4.5.0
-
-ncrename -v TRNO3_bioalk,TRNO3_TA accord_bgc_ini.nc
-ncrename -v TRBO3_bioalk,TRBO3_TA accord_bgc_ini.nc
-
-
---
+++ create_IC.sh
@@ -0,0 +1,8 @@
+
+#Load modules
+module unload cray-netcdf-hdf5parallel cray-hdf5-parallel
+module load cray-netcdf cray-hdf5
+module load nco/4.5.0
+module load anaconda/python3
+
+
